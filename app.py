46cc6f28433d9ea24f26b581bc9692d3209a0c86
kh7230 <wnrkdgml7730@gmail.com>
2023-05-09 16:45:37 +0900

    updated

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from description import run_description
 from eda import run_eda
+from ml_app import run_ml_app
 from stats import run_stat
 
 def main():
     with st.sidebar:
-        selected = option_menu("Main Menu", ['Home', 'Description', 'Data', 'EDA', 'STAT'],
+        selected = option_menu("Main Menu", ['Home', 'Description', 'Data', 'EDA','ML_APP', 'STAT'],
                 icons=['house', 'card-checklist', 'card-checklist', 'bar-chart', 'clipboard-data'],
                 menu_icon="cast", default_index=1, orientation = 'vertical')
 
@@ -17,6 +18,8 @@ def main():
         pass
     elif selected == 'EDA':
         run_eda()
+    elif selected == 'ML_APP':
+        run_ml_app()
     elif selected == 'STAT':
         run_stat()
     else:
