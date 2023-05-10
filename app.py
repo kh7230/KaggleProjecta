0e6d2f129b477705103812d1b88ca263b1da15ad
kh7230 <wnrkdgml7730@gmail.com>
2023-05-10 09:41:08 +0900

    updated

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from eda import run_eda
 from ml_app import run_ml_app
 from stats import run_stat
 
-def main(run_lecture=None):
+def main():
     with st.sidebar:
-        selected = option_menu("Main Menu", ['Home', 'Description', 'Data', 'EDA','ML_APP', 'STAT','LECTURE'],
+        selected = option_menu("Main Menu", ['Home', 'Description', 'Data', 'EDA','ML_APP', 'STAT'],
                 icons=['house', 'card-checklist', 'card-checklist', 'bar-chart', 'clipboard-data'],
                 menu_icon="cast", default_index=0, orientation = 'vertical')
 
@@ -22,8 +22,6 @@ def main(run_lecture=None):
         run_ml_app()
     elif selected == 'STAT':
         run_stat()
-    elif selected == 'LECTURE':
-        run_lecture()
     else:
         print('error..')
 
