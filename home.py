a03480d551cf406e84f9d38fa35145b87c3d8c84
kh7230 <wnrkdgml7730@gmail.com>
2023-05-15 09:25:00 +0900

    updated

--- home.py
+++ home.py
@@ -5,7 +5,3 @@ def run_home():
     st.markdown("# MARMOT")
     img = Image.open('datas/image_1.jpg')
     st.image(img)
-
-width, height = img.size
-smaill = img.resize((width//2,height//2), Image.ANTIALIAS)
-smaill.save('datas/image_1.jpg', 'jpg')
